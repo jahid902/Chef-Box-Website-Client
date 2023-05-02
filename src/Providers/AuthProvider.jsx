9a12a3dee4d1b1e2a51d47a5b1549946d5882881
jahid902 <jahidjr902@gmail.com>
2023-05-02 20:25:53 +0600

    working on authentication

---
+++ src/Providers/AuthProvider.jsx
@@ -0,0 +1,24 @@
+import React, { createContext } from 'react';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+
+const AuthProvider = ({children}) => {
+
+    const registerUser = (email,password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const authInfo = {
+        registerUser
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
