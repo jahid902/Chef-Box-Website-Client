6e613ef028293311edbc1f311ee536176bf36568
jahid902 <jahidjr902@gmail.com>
2023-05-02 19:53:10 +0600

    added login register options

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
-    return (
-        <div>
-            login page
+
+    const [error, setError] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email,password);
+        form.reset();
+    }
+
+  return (
+    <div className="hero min-h-screen bg-base-200 font-serif">
+      <div className="hero-content flex-col">
+        <div className="text-center lg:text-left">
+          <h1 className="text-4xl font-bold">Please login</h1>
         </div>
-    );
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form onSubmit={handleSubmit} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                Don't have an account?
+                <Link
+                  to="/register"
+                  className="label-text-alt text-blue-600 font-bold text-base link link-hover"
+                >
+                  Register here
+                </Link>
+              </label>
+            </div>
+            <p className="text-red-700 text-lg">{error}</p>
+            <div className="form-control mt-6">
+              <button className="btn btn-common">Login</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -1,10 +1,92 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const RegisterPage = () => {
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const image = form.image.value;
+        const password = form.password.value;
+        console.log(name, email, image, password);
+        form.reset()
+    }
+
     return (
-        <div>
-            register page
+        <div className="hero min-h-screen bg-base-200 font-serif">
+      <div className="hero-content flex-col">
+        <div className="text-center lg:text-left">
+          <h1 className="text-4xl font-bold">Please Register</h1>
         </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form onSubmit={handleSubmit}  className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="your name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Image</span>
+              </label>
+              <input
+                type="text"
+                name="image"
+                placeholder="Image Url"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                Already have an account?
+                <Link
+                  to="/login"
+                  className="label-text-alt text-blue-600 font-bold text-base link link-hover"
+                >
+                  Login here
+                </Link>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-common">Register</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
     );
 };
 
